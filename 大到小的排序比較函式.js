878aa354372b7274b23cf3efe270f1a7a9867f09
chenbinzhou <jou97101@gmail.com>
2021-10-19 23:59:48 +0800

    認可

--- 大到小的排序比較函式.js
+++ 大到小的排序比較函式.js
@@ -4,17 +4,17 @@ var poker=[];
 for(var i=0;i<52;i++){
     switch(parseInt(i/13)){
         case 0:
-            poker.push("♥"+(parseInt(i%13)+1));
+            poker.push("H"+(parseInt(i%13)+1));
             
             break;
         case 1:
-            poker.push("♠"+(parseInt(i%13)+1));
+            poker.push("S"+(parseInt(i%13)+1));
             break;
         case 2:
-            poker.push("♣"+(parseInt(i%13)+1));
+            poker.push("C"+(parseInt(i%13)+1));
             break;
         case 3:
-            poker.push("♦"+(parseInt(i%13)+1));
+            poker.push("D"+(parseInt(i%13)+1));
     }
    
 }
@@ -45,26 +45,25 @@ for (let i = 0; i < poker.length; ) {
     
 }
 function compare(a,b){
-    if(b.charCodeAt(0) == a.charCodeAt(0) )//同花色
-     //compare charAt(1) charAt(2)同花色 不同數字 排序    
-    {  return b.slice(1,b.length)-a.slice(1,a.length);}//
+    if(b.charCodeAt(0) == a.charCodeAt(0))//同花色 不同數字 大小排序  
+    {  return b.slice(1,b.length)-a.slice(1,a.length);}
 
-    else{ return b.charCodeAt(0) - a.charCodeAt(0)//不同花色排序 順序為黑桃,梅花,愛心,方塊
+    else{ return a.charCodeAt(0) - b.charCodeAt(0)//不同花色排序 順序為黑桃,梅花,愛心,方塊
         }
 }
 
-console.log("Player1:"+player1)
+//console.log("Player1:"+player1)
 player1.sort(compare);
 console.log("Player1:"+player1)
 
-console.log("Player2:"+player2)
+//console.log("Player2:"+player2)
 player2.sort(compare)
 console.log("Player2:"+player2)
 
-console.log("Player3:"+player3)
+//console.log("Player3:"+player3)
 player3.sort(compare)
 console.log("Player3:"+player3)
 
-console.log("Player4:"+player4)
+//console.log("Player4:"+player4)
 player4.sort(compare)
 console.log("Player4:"+player4)
