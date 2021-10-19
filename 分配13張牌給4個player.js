b1a635407ed024935327a522971c000baecaeef1
chenbinzhou <jou97101@gmail.com>
2021-10-20 00:27:16 +0800

    認可

--- 分配13張牌給4個player.js
+++ 分配13張牌給4個player.js
@@ -17,6 +17,14 @@ for(var i=0;i<52;i++){
 }
 console.log(poker.toString());
 var player1=[], player2=[], player3=[],player4=[];
+//大小排序函式
+function sortnumber(a,b){
+    if(b.charCodeAt(0) == a.charCodeAt(0)) 
+    {  return b.slice(1,b.length)-a.slice(1,a.length);}
+
+    else{ return b.charCodeAt(0) - a.charCodeAt(0)
+        }
+}
 
 function compare(a,b){
         return Math.random()-0.5;
@@ -29,62 +37,22 @@ console.log(poker.toString());
 //發牌給玩家1
 for(var x=0,y=0; y<13; x++){   
     player1[x]=poker[y++];}
-
-    //玩家1手牌大小排序
-    player1.sort(function(a, b){
-        if(b.charCodeAt(0) == a.charCodeAt(0))
-          
-        {  return b.slice(1,b.length)-a.slice(1,a.length);}
-    
-        else{ return b.charCodeAt(0) - a.charCodeAt(0)
-            }
-    }
-      );
+    player1.sort(sortnumber);
     console.log("Player1:"+player1)
 
 //發牌給玩家2
 for(var x=0,y=13; y<26; x++){  
     player2[x]=poker[y++];}
-
-    //玩家2手牌大小排序
-    player2.sort(function(a, b){
-        if(b.charCodeAt(0) == a.charCodeAt(0))
-          
-        {  return b.slice(1,b.length)-a.slice(1,a.length);}
-    
-        else{ return b.charCodeAt(0) - a.charCodeAt(0)
-            }
-    }
-      );
+    player2.sort(sortnumber);
     console.log("Player2:"+player2)
 
 //發牌給玩家3
 for(var x=0,y=26; y<39; x++){  
         player3[x]=poker[y++];}
-
-        //玩家3手牌大小排序
-    player1.sort(function(a, b){
-        if(b.charCodeAt(0) == a.charCodeAt(0))
-          
-        {  return b.slice(1,b.length)-a.slice(1,a.length);}
-    
-        else{ return b.charCodeAt(0) - a.charCodeAt(0)
-            }
-    }
-      );
+        player3.sort(sortnumber);
         console.log("Player3:"+player3)
 //發牌給玩家4
 for(var x=0,y=39; y<52; x++){  
         player4[x]=poker[y++];}
-
-        //玩家4手牌大小排序
-    player1.sort(function(a, b){
-        if(b.charCodeAt(0) == a.charCodeAt(0))
-          
-        {  return b.slice(1,b.length)-a.slice(1,a.length);}
-    
-        else{ return b.charCodeAt(0) - a.charCodeAt(0)
-            }
-    }
-      );
+        player4.sort(sortnumber);
         console.log("Player4:"+player4)
